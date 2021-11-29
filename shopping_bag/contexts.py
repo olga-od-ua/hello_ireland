994a7109f73a13f036f17cb616d13338a8c45d66
olga-od-ua <olgasokolova21@gmail.com>
2021-11-29 15:32:42 +0000

    Add shopping bag items iteration in contexts.py

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def shopping_bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    for item_id, quantity in shopping_bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_SHIPPING_THRESHOLD:
         shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
