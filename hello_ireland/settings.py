8998bc0979cf49810d3dd45092364444582547a0
olga-od-ua <olgasokolova21@gmail.com>
2021-12-10 23:30:02 +0000

    Set up database for deployment

--- hello_ireland/settings.py
+++ hello_ireland/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -122,8 +123,8 @@ WSGI_APPLICATION = 'hello_ireland.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
