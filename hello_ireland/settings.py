b4dcb9476c6f34b7848f9958355af5451bc30905
olga-od-ua <olgasokolova21@gmail.com>
2021-12-11 18:18:42 +0000

    Remove secret key and set debug

--- hello_ireland/settings.py
+++ hello_ireland/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(&sr05=^@bsuiz!kgm)$y1sm2pq7zc&_hn5*jxv^sq+-u(c_er'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['oh-hello-ireland.herokuapp.com', 'localhost']
 
