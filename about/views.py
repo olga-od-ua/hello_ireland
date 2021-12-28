a2c0c5260b82e3f1d0337c5547136bdced05e55b
olga-od-ua <olgasokolova21@gmail.com>
2021-12-28 16:47:59 +0000

    Add positive reviews to about us page placed in an accordion, update About Us info

--- about/views.py
+++ about/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from products.models import ProductReview, Product
+
 # Create your views here.
 """ A view to render the About Us page """
 
 def about(request):
-    return render(request, "about/about_us.html")
+    reviews = ProductReview.objects.all()
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, "about/about_us.html", context)
