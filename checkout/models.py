bb4f460c71ff50d9cecc8552486cace3e3202097
olga-od-ua <olgasokolova21@gmail.com>
2021-12-30 10:38:00 +0000

    Add fields to favourite products, style favourite product cards, add floatformat filter to confirmation email body.

--- checkout/models.py
+++ checkout/models.py
@@ -43,7 +43,7 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_SHIPPING_THRESHOLD:
-            self.shipping_cost = self.order_total * settings.STANDARD_SHIPPING_PERCENTAGE / 100
+            self.shipping_cost = ((self.order_total * settings.STANDARD_SHIPPING_PERCENTAGE) / 100)
         else:
             self.shipping_cost = 0
         self.grand_total = self.order_total + self.shipping_cost
