7a44000ba60b39a86b39faafde6e8818cb4e4cfc
olga-od-ua <olgasokolova21@gmail.com>
2022-01-01 12:44:32 +0000

    Home app: fix html indentation, pylint and flake8 errors, perform spell check.

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,8 @@
+""" A Module for application configuration for the Home app """
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """ A Class for application configuration for the Home app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
+""" A Module to create the Home Page views """
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
+    """ A view to rended the Home Page """
 
     return render(request, "home/index.html")
